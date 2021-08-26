447968cc535a90d87abe464c55adcdbd31c5bd84
LIUAofStevensTransport <58896250+LIUAofStevensTransport@users.noreply.github.com>
2021-08-26 16:24:50 +0300

    Capital.com clock
    Final

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,14 @@
+// This clock page was made by Andrei Liubiski on 25 Aug 2021 as a part of training on '30 days of JS' by Wes Bos 
+// on https://courses.wesbos.com/ 
+// The logo is of Capital.com 
+// The gulp build is by Coder Coder from https://youtu.be/q0E1hbcj-NI
+
+// This page is accessible from my github account (https://github.com/LIUAofStevensTransport/30daysOfJS_Day_2_Clock_Capital.com)
+//  and my website (http://www.liubinski.com/projects/30DaysOfJS/30daysOfJS_Day_2_Clock_Capital.com/)
+
+
+
+
 const secondsHand = document.querySelector('.seconds');
 const minutesHand = document.querySelector('.minute');
 const hoursHand = document.querySelector('.hour');
